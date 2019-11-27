c725dc97d212c88f485d93b4c1f5380f84310d77
JamesBerryman <50531563+JamesBerryman@users.noreply.github.com>
2019-11-27 18:33:24 +0000

    Updated Pagelinks
    Pagelinks positioning update, added link to Logo. Rounded Youtube Height calucuation to prevent frezzing on different sized screens.

--- javascript.js
+++ javascript.js
@@ -29,8 +29,9 @@ function showSlides(n) {
 function youTubeAspect() {
   //will calculate the size of a youtube video so it fits the article box.
   var articleWidth = document.getElementsByClassName('toparticle')[0].offsetWidth - 40; //you have 20px padding, got to take 40px of total width.
-  var videoHeight = (articleWidth / 16)*9;
+  var videoHeight = (Math.floor(articleWidth / 16)*9);
   console.log(articleWidth);
+  console.log(videoHeight);
   var youTubeVideos = document.getElementsByClassName("youTubeClip");
   for (i = 0; i < youTubeVideos.length; i++) {
     youTubeVideos[i].style.width = articleWidth+"px";
